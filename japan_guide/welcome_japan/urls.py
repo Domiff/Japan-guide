50941becc23f9865a35db66657035dfbe7fcd6b5
Domitori <dmlegasy@gmail.com>
2025-04-12 20:57:07 +0300

    Was added page of Tokyo

--- japan_guide/welcome_japan/urls.py
+++ japan_guide/welcome_japan/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import WelcomeView, AboutTokyoView
+from .views import (WelcomeView,
+                    AboutTokyoView,
+                    PopularPlacesView,
+                    TokyoView,
+                    )
 
 urlpatterns = [
     path('', WelcomeView.as_view(), name='welcome'),
     path('about', AboutTokyoView.as_view(), name='about'),
+    path('places', PopularPlacesView.as_view(), name='places'),
+    path('tokyo', TokyoView.as_view(), name='tokyo'),
 ]
--- japan_guide/welcome_japan/views.py
+++ japan_guide/welcome_japan/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.shortcuts import render
 from django.views.generic import View
 
 
 class WelcomeView(View):
     def get(self, request):
-        # if request.method == 'GET':
-        #     url = reverse(request.path)
-        #     return redirect(url)
         return render(request, 'welcome_japan/welcome.html')
 
 
 class AboutTokyoView(View):
     def get(self, request):
-        return render(request, 'welcome_japan/about_tokyo.html')
+        return render(request, 'welcome_japan/about_japan.html')
+
+
+class PopularPlacesView(View):
+    def get(self, request):
+        return render(request, 'welcome_japan/popular_places.html')
+
+
+class TokyoView(View):
+    def get(self, request):
+        return render(request, 'welcome_japan/tokyo.html')
